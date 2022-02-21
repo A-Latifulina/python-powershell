0bfb89b75d9a00afcf168168b7dbc951df7a5523
Adelya <adelya@astar.app>
2022-02-20 21:40:13 -0500

    Add files via upload

---
+++ lab5.ps1
@@ -0,0 +1,20 @@
+﻿
+#install OpenSSH
+powershell.exe -ExecutionPolicy Bypass -File install-sshd.ps1
+
+# Start the sshd service
+Start-Service sshd
+Start-Service ssh-agent
+
+get-service sshd
+get-service ssh-agent
+
+Get-NetFirewallRule -Name *ssh*
+New-NetFirewallRule -Protocol TCP -LocalPort 22 -Direction Inbound -Action Allow -DisplayName SSH
+New-NetFirewallRule -Protocol TCP -LocalPort 22 -Direction Outbound -Action Allow -DisplayName SSH
+
+stop-service sshd
+stop-service ssh-agent
+
+
+
