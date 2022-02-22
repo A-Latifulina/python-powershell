113b9b429c1df6b0246838429923813f4a69b7d7
Adelya <adelya@astar.app>
2022-02-21 22:49:47 -0500

    Add files via upload

---
+++ task31.ps1
@@ -0,0 +1,4 @@
+﻿#verify data integrity with hashing
+
+Get-FileHash .\task12.ps1 |Format-List
+Get-FileHash .\task21.py |Format-List
---
+++ task33.ps1
@@ -0,0 +1,22 @@
+﻿#self signing 
+$selfcert = New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\My -DnsName "AdelyaCertCYB"
+
+#8DC7EA685B4A984356B39F629557A3AFF7D46AD9  CN=AdelyaCert
+
+$rootstore = [System.Security.Cryptography.X509Certificates.X509Store]::new("Root", "LocalMachine")
+$rootstore.Open("ReadWrite")
+$rootstore.Add($selfcert)
+$rootstore.Close()
+
+$publishstore = [System.Security.Cryptography.X509Certificates.X509Store]::new("TrustedPublisher", "LocalMachine")
+$publishstore.Open("ReadWrite")
+$publishstore.Add($selfcert)
+$publishstore.Close()
+
+$codecertificate = Get-ChildItem Cert:\LocalMachine\My |Where-Object{$_.Subject -eq "CN=AdelyaCertCYB"}
+
+$codecertificate
+
+Set-AuthenticodeSignature -FilePath .\task11.ps1 -Certificate $codecertificate -TimestampServer http://timestamp.digicert.com
+
+
